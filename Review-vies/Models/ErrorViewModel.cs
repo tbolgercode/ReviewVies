2d3b30d88455a12ec7f67553432cc191a1e1cc79
Tom Bolger <tbolgercode@gmail.com>
2021-02-15 17:17:35 -0600

    Committing base project

---
+++ Review-vies/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Review_vies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
